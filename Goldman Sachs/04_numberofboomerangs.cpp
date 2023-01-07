fc5ff90ebe686b0f1ba32aa6034ffa06435cfa36
Arjya <arjyabhattacharjee5@gmail.com>
2023-01-07 23:41:36 +0530

    Added files

---
+++ Goldman Sachs/04_numberofboomerangs.cpp
@@ -0,0 +1,21 @@
+//Number of Boomerangs
+
+class Solution {
+public:
+    int numberOfBoomerangs(vector<vector<int>>& points) {
+      int n=points.size();
+      int total=0;
+      for(int i=0;i<n;i++){
+          map<int,int>mp;
+          for(int j=0;j<n;j++){
+              int distance=((points[i][0]-points[j][0])*(points[i][0]-points[j][0]))+((points[i][1]-points[j][1])*(points[i][1]-points[j][1]));
+              mp[distance]++;
+          }
+          for(auto i : mp){
+              if(i.second) 
+              total+=(i.second*(i.second-1));
+          }
+      } 
+      return total; 
+    }
+};
