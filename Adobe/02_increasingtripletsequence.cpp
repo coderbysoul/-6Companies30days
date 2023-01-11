3cc9fd588777797bd444014c797133dcbced9d21
Arjya <arjyabhattacharjee5@gmail.com>
2023-01-11 19:24:42 +0530

    Started Adobe

---
+++ Adobe/02_increasingtripletsequence.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/increasing-triplet-subsequence/description/
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int left=INT_MAX;
+        int mid=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<left)
+            left=nums[i];
+            if(nums[i]>left&&nums[i]<mid)
+            mid=nums[i];
+            if(nums[i]>mid)
+            return true;
+        }
+        return false;
+    }
+};
