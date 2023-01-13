164fd3fb97a35e055b4e9aa43afc18d9b9623a3e
Arjya <arjyabhattacharjee5@gmail.com>
2023-01-13 19:59:56 +0530

    Completed third company Adobe

---
+++ Adobe/11_stockpricefluctuation.cpp
@@ -0,0 +1,53 @@
+//Stock Price Fluctuation
+
+class StockPrice
+{
+public:
+    unordered_map<int, int> map;
+    priority_queue<pair<int, int>> PQ;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> PQ_MIN;
+
+    int currentPrice = 0;
+    int currentTime = 0;
+
+    StockPrice() {}
+    void update(int timestamp, int price)
+    {
+
+        map[timestamp] = price;
+
+        PQ.push({price, timestamp});
+        PQ_MIN.push({price, timestamp});
+
+        
+        if (timestamp >= currentTime)
+        {
+            currentTime = timestamp;
+            currentPrice = price;
+        }
+        
+        while (map[PQ.top().second] != PQ.top().first)
+        {
+            PQ.pop();
+        }
+        
+        while (map[PQ_MIN.top().second] != PQ_MIN.top().first)
+        {
+            PQ_MIN.pop();
+        }
+    }
+    int current()
+    {
+        return currentPrice;
+    }
+
+    int maximum()
+    {
+        return PQ.top().first;
+    }
+
+    int minimum()
+    {
+        return PQ_MIN.top().first;
+    }
+};
